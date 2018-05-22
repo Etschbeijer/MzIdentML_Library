222d7a0e79fd848f76d878545c7d0f814f2fd199
patrick blume <PatrickBlume@gmx.de>
2018-05-22 16:25:01 +0200

    Update

--- MzIdentML_Library/InsertStatements_EasyVersion.fs
+++ MzIdentML_Library/InsertStatements_EasyVersion.fs
@@ -2405,6 +2405,55 @@ module InsertStatements =
                         (addToContextWithExceptionCheck context item) |> ignore
                         insertWithExceptionCheck context
 
+        module initializeStandardDB =
+            open BioFSharp.IO
+            open FSharp.Care.IO
+
+            ///Define reader for OboFile
+            let fromFileObo (filePath : string) =
+                FileIO.readFile filePath
+                |> Obo.parseOboTerms
+
+            let fromPsiMS =
+                fromFileObo (fileDir + "\Ontologies\Psi-MS.txt")
+
+            let fromPride =
+                fromFileObo (fileDir + "\Ontologies\Pride.txt")
+
+            let fromUniMod =
+                fromFileObo (fileDir + "\Ontologies\Unimod.txt")
+
+            let fromUnit_Ontology =
+                fromFileObo (fileDir + "\Ontologies\Unit_Ontology.txt")
+
+            let initStandardDB (context : MzIdentMLContext) =
+                let terms_PsiMS =
+                    fromPsiMS
+                    |> Seq.map (fun termItem -> TermHandler.init(termItem.Id, termItem.Name))
+                let psims = OntologyHandler.init ("Psi-MS", terms_PsiMS)
+                OntologyHandler.addToContext context psims |> ignore
+
+                let terms_Pride =
+                    fromPsiMS
+                    |> Seq.map (fun termItem -> TermHandler.init(termItem.Id, termItem.Name))
+                let psims = OntologyHandler.init ("Pride", terms_Pride)
+                OntologyHandler.addToContext context psims |> ignore
+
+                let terms_Unimod =
+                    fromPsiMS
+                    |> Seq.map (fun termItem -> TermHandler.init(termItem.Id, termItem.Name))
+                let psims = OntologyHandler.init ("Unimod", terms_Unimod)
+                OntologyHandler.addToContext context psims |> ignore
+
+                let terms_Unit_Ontology =
+                    fromPsiMS
+                    |> Seq.map (fun termItem -> TermHandler.init(termItem.Id, termItem.Name))
+                let psims = OntologyHandler.init ("Unit_Ontology", terms_Unit_Ontology)
+                OntologyHandler.addToContext context psims |> ignore
+
+                context.Database.EnsureCreated() |> ignore
+                context.SaveChanges()
+
     module test =
     //Apply functions
         
